cdff8a7b39343abb0796d2991d906d87794f22b3
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-02-21 19:42:55 +0300

    окно с надписью на русском

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    let font = load_ttf_font("assets/Inter_18pt-Bold.ttf").await.unwrap();
+    loop {
+        clear_background(DARKGRAY);
+        draw_text_ex(
+            "симулятор",
+            20.0,
+            20.0,
+            TextParams {
+                font: Some(&font),
+                font_size: 18,
+                color: BLACK,
+                ..Default::default()
+            },
+        );
+
+        next_frame().await
+    }
 }
